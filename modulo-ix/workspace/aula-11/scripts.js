0039a48fe2450a932d092a0d7c862553a5545df9
danilocdesousa <45634291+danilocdesousa@users.noreply.github.com>
2022-07-16 18:10:23 -0300

    Conclui Módulo IX - Aula 11

---
+++ modulo-ix/workspace/aula-11/scripts.js
@@ -0,0 +1,27 @@
+"use strict";
+
+class Product {
+    description;
+    width;
+    height;
+    depth;
+    value;
+    constructor(description, width, height, depth, value) {
+        this.description = description;
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.value = value;
+    }
+    productVolume() {
+        return this.width * this.height * this.depth;
+    }
+
+    installmentAmount(installment) {
+        return this.value / installment;
+    }
+}
+
+const product1 = new Product("Product 1", 2, 4, 6, 10);
+console.log(product1.productVolume());
+console.log(product1.installmentAmount(2));
