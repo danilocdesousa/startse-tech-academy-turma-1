c93066307c1a0d1d246031db6e6fa50f6539130b
danilocdesousa <45634291+danilocdesousa@users.noreply.github.com>
2022-07-19 00:18:23 -0300

    Conclui Módulo IX - Aula 14

---
+++ modulo-ix/workspace/aula-14/scripts.js
@@ -0,0 +1,33 @@
+"use strict";
+/* JavaScript Class super */
+
+class Product {
+    type;
+    constructor(type) {
+        this.type = type;
+    }
+    getData() {
+        return `Type: ${this.type}`;
+    }
+}
+
+class Book extends Product {
+    title;
+    numberOfPages;
+    constructor(type, title, numberOfPages) {
+        // super(type);
+        super("Book");
+        this.title = title;
+        this.numberOfPages = numberOfPages;
+        console.log(this.getData());
+        // console.log(super.getData());
+    }
+    showBookData() {
+        console.log(`${super.getData()}, ${this.title}, ${this.numberOfPages}`);
+    }
+}
+
+const product1 = new Product("Product 1");
+// console.log(product1.getData());
+const book1 = new Book("Product", "Book A", 199);
+book1.showBookData();
